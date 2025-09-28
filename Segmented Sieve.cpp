3b1c969ac95db291a29530ddfebd1253316f2241
mdmifat-369 <mdmifat369@gmail.com>
2025-09-28 11:12:46 +0600

    Add files via upload

--- Segmented Sieve.cpp
+++ Segmented Sieve.cpp
@@ -1,18 +1,8 @@
-/*
-                In the name of Allah, The Most Gracious and The Most Merciful.
-           Glory be to You(Allah)! We have no knowledge except what You have taught us.
-                         It is you who are the Knowledgeable, the Wise.
-           ***************************************************************************/
-
 #include<bits/stdc++.h>
-//#include<ext/pb_ds/assoc_container.hpp>
-//#include<ext/pb_ds/tree_policy.hpp>
-#define mod 1000000007
-#define int long long int
 using namespace std;
-//using namespace __gnu_pbds;
-const int N = 1e6+10;
-//template<typename T> using pbds= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+#define int long long
+const int N=1e6+10;
+
 vector<int>primeNum(N,0);
 vector<int>v;
 
@@ -34,13 +24,13 @@ void sieve(){
 }
 
 int segment_sieve(int L,int R){
-     if(L==1)L++;
-     int mx=R-L+1;
-     int ara[mx];
-     memset(ara,0,sizeof(ara));
+    if(L==1)L++;
+    int mx=R-L+1;
+    int ara[mx+1];
+    memset(ara,0,sizeof(ara));
 
-     for(int p:v){
-         if(p*p<=R){
+    for(int p:v){
+        if(p*p<=R){
             int i=(L/p)*p;
             if(i<L)i+=p;
 
@@ -49,19 +39,19 @@ int segment_sieve(int L,int R){
                     ara[i-L]=1;
                 }
             }
-         }
-     }
-
-     int cnt=0;
-     for(int i=0;i<mx;i++){
-        if(ara[i]==0){
-            cnt++;
-            cout<< L+i <<" ";
         }
-     }
-     cout<<endl;
+    }
 
-     return cnt;
+    int cnt=0;
+    for(int i=0;i<mx;i++){
+        if(ara[i]==0){
+            cout<< i+L <<" ";
+            cnt++;
+        }
+    }
+    cout<<endl;
+
+    return cnt;
 }
 
 void solve(){
@@ -71,17 +61,16 @@ void solve(){
     cout<< segment_sieve(L,R) <<endl;
 }
 
-int32_t main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int32_t main(){
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+      
+      sieve();
 
-    sieve();
+      int t;
+      cin>>t;
 
-    int t;
-    cin>>t;
-    while(t--)
-    solve();
-    
-    return 0;
+      while(t--){
+        solve();
+      }
 }
