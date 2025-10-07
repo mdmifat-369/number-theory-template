ac531c1b1f45f9632a65d19b1f12863ade12aae1
mdmifat-369 <mdmifat369@gmail.com>
2025-10-07 16:12:25 +0600

    Add files via upload

--- Query base  Binomial Coefficient using Modulo.cpp
+++ Query base  Binomial Coefficient using Modulo.cpp
@@ -2,32 +2,34 @@
 using namespace std;
 #define int long long int
 #define mod 1000000007
-const int N=1e6;
+const int N=1000;
 int fact[N];
 int insfact[N];
 
-int binExp(int a, int b) {
-    int res = 1;
-    while (b > 0) {
-        if (b & 1) res = (res * a) % mod;
-        a = (a * a) % mod;
-        b >>= 1;
+int binExp(int base, int POW) {
+    int res=1;
+    while(POW>0){
+        if(POW%2==1){
+            POW--;
+            res=(res*base)%mod;
+        }else{
+            POW/=2;
+            base=(base*base)%mod;
+        }
     }
-    return res;
+
+    return res%mod;
 }
 
-int inverse(int a){
-   return binExp(a,mod-2);
-}
-
-void precompute_fact(){
+void precompute(){
     fact[0]=1;
     for(int i=1;i<=N;i++){
         fact[i]=(fact[i-1]*i)%mod;
     }
 
-    for(int i=0;i<=N;i++){
-        insfact[i]=inverse(fact[i]);
+    insfact[N-1]=binExp(fact[N-1],mod-2);
+    for(int i=N-2;i>=0;i--){
+        insfact[i]=(insfact[i+1]*(i+1))%mod;
     }
 }
 
@@ -35,19 +37,19 @@ void solve(){
     int n,r;
     cin>>n>>r;
 
-        if(r<0 || r>n){
+      if(r<0 || r>n){
         cout<<0<<"\n";
         return;
     }
 
-    cout<< (fact[n]*insfact[r]%mod*insfact[n-r]%mod)%mod <<endl;
+    cout<< (fact[n]*insfact[r]%mod*insfact[n-r]%mod)%mod;
 }
 
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    precompute_fact();
+    precompute();
     int t;
     cin>>t;
 
